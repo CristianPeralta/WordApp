de7595f2bccabd539c3f99f2f55a18286020fc97
Cristian Peralta <cbperaltasegura@gmail.com>
2018-03-25 09:08:10 -0500

    separate result in colum

--- src/components/Searcher.vue
+++ src/components/Searcher.vue
@@ -20,14 +20,23 @@
         </a>
       </p>
     </div>
-
     <div style="text-align: center; font-size: 30px;">
-      <template  v-for="(item, index) in hyphenation">
-        <template v-if="item.type=='stress'">
-          <strong :key="index">{{item.text}} </strong>
-        </template>
-        <span v-else :key="index">{{item.text}} </span>
-      </template>
+      <div class="columns">
+        <div class="column"></div>
+        <div class="column">
+          <template  v-for="(item, index) in hyphenation">
+            <template v-if="item.type=='stress'">
+              <strong :key="index">{{item.text}} </strong>
+            </template>
+            <span v-else :key="index">{{item.text}} </span>
+          </template>
+        </div>
+        <div class="column">
+          <div ref="audio"></div>
+        </div>
+        <div class="column"></div>
+      </div>
+
     </div>
     <ul>
       <li v-for="(item, index) in definitions" :key="index">
@@ -121,6 +130,8 @@ export default {
       }).then((response) => {
         this.antonym = response.data[0]
       })
+    },
+    getAudio () {
     }
   }
 }
