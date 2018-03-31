864cc0912d5827768fc584f76edadf6040b38a33
Cristian Peralta <cbperaltasegura@gmail.com>
2018-03-31 09:33:13 -0500

    add functions to hide or show eamples definitions and count

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -17,15 +17,15 @@
         </div>
 
         <article class="media">
-          <figure class="media-left">
+          <figure @click="showDefinitions" class="media-left">
             <p class="image is-48x48">
               <img src="https://www.flaticon.com/premium-icon/icons/svg/797/797446.svg">
             </p>
           </figure>
-          <div class="media-content">
+          <div @click="showDefinitions" class="media-content">
             <div class="content">
-              <strong>Definitions</strong>
-              <li v-for="(item, index) in word.definitions" :key="index">
+              <strong>Definitions ({{word.definitions | count}})</strong>
+              <li v-if="defStatus" v-for="(item, index) in word.definitions" :key="index">
                 <Card :source="item.partOfSpeech" :text="item.text" :attribution="item.source"></Card>
               </li>
             </div>
@@ -33,16 +33,16 @@
         </article>
 
         <article class="media" v-if="word.examples">
-          <figure class="media-left">
+          <figure @click="showExamples" class="media-left">
             <p class="image is-48x48">
               <img src="https://image.flaticon.com/icons/svg/468/468045.svg">
             </p>
           </figure>
-          <div class="media-content">
+          <div @click="showExamples" class="media-content">
             <div class="content">
-              <strong>Examples </strong>
+              <strong>Examples ({{word.examples | count}})</strong>
               <br>
-              <li v-for="(item, index) in word.examples" :key="index">
+              <li v-if="exStatus" v-for="(item, index) in word.examples" :key="index">
                 <Card :source="item.title" :text="item.text"></Card>
               </li>
               <br>
@@ -60,6 +60,25 @@ export default {
   props: ['word'],
   components: {
     Card
+  },
+  data () {
+    return {
+      defStatus: true,
+      exStatus: true
+    }
+  },
+  filters: {
+    count (value) {
+      return value.length
+    }
+  },
+  methods: {
+    showDefinitions () {
+      this.defStatus = !this.defStatus
+    },
+    showExamples () {
+      this.exStatus = !this.exStatus
+    }
   }
 }
 </script>
